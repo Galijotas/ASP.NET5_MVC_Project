8155d191b67a54c3577b90cc93f30885b5aa0f48
DESKTOP-66K2L8E\Lukas <lgalijotas@gmail.com>
2023-03-09 21:07:35 +0200

    Login, Register views

---
+++ MagicVilla/MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string _villaUrl;
+
+        public AuthService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = obj,
+                Url = _villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegistrationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = obj,
+                Url = _villaUrl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
